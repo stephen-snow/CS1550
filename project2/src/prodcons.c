5d0227ba6625ce0e9257686ce34fbb0935390fd5
Stephen C Snow <scs89@thoth.cs.pitt.edu>
2016-10-16 17:20:46 -0400

    Example with forking

--- project2/src/prodcons.c
+++ project2/src/prodcons.c
@@ -11,11 +11,14 @@
 #include <linux/prodcons.h>
 
 void *BASE_PTR;
-
-
-void main(int aegc, char *argv[])
-{
-
+void main(int argc, char *argv[]) {
+     if (argc < 4) {
+          printf("Not enough arguments");
+          exit(1);
+     }
+     const int ncons = atoi(argv[1]);
+     const int nprod = atoi(argv[2]);
+     const int n = atoi(argv[3]);
      BASE_PTR = (void *) mmap(NULL, MAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0);
      if(BASE_PTR == (void *) -1) 
      {
@@ -26,8 +29,9 @@ void main(int aegc, char *argv[])
      int *base_ptr = BASE_PTR;
      int *new_ptr;
      int *curr_ptr = BASE_PTR;
-     int size = sizeof(struct cs1550_sem);
-     curr_ptr = curr_ptr + size;
+     int semsize = sizeof(struct cs1550_sem);
+     // Empty semaphore
+     curr_ptr = curr_ptr + semsize;
      if(curr_ptr > base_ptr + MAP_SIZE) 
      {
           fprintf(stderr, "Address out of range\n");
@@ -35,12 +39,107 @@ void main(int aegc, char *argv[])
      }
      else
      {
-          new_ptr = curr_ptr - size;
+          new_ptr = curr_ptr - semsize;
+     }
+     struct cs1550_sem *empty = (struct cs1550_sem *) new_ptr;
+     empty->value = n;
+     // Full semaphore
+     curr_ptr = curr_ptr + semsize;
+     if(curr_ptr > base_ptr + MAP_SIZE) 
+     {
+          fprintf(stderr, "Address out of range\n");
+          exit(1);
+     }
+     else
+     {
+          new_ptr = curr_ptr - semsize;
+     }
+     struct cs1550_sem *full = (struct cs1550_sem *) new_ptr;
+     full->value = 0;
+     // Mutex semaphore
+     curr_ptr = curr_ptr + semsize;
+     if(curr_ptr > base_ptr + MAP_SIZE) 
+     {
+          fprintf(stderr, "Address out of range\n");
+          exit(1);
+     }
+     else
+     {
+          new_ptr = curr_ptr - semsize;
+     }
+     struct cs1550_sem *mutex = (struct cs1550_sem *) new_ptr;
+     mutex->value = 1;
+
+     // Pancakes
+     curr_ptr = curr_ptr + n * sizeof(int);
+     if(curr_ptr > base_ptr + MAP_SIZE) 
+     {
+          fprintf(stderr, "Address out of range\n");
+          exit(1);
+     }
+     else
+     {
+          new_ptr = curr_ptr - n * sizeof(int);
+     }
+     int *buffer = (int *) new_ptr;
+
+     // in position
+     curr_ptr = curr_ptr + sizeof(int);
+     if(curr_ptr > base_ptr + MAP_SIZE) 
+     {
+          fprintf(stderr, "Address out of range\n");
+          exit(1);
+     }
+     else
+     {
+          new_ptr = curr_ptr - sizeof(int);
+     }
+     int *in = (int *) new_ptr;
+     *in = 0;
+     // out position
+     curr_ptr = curr_ptr + sizeof(int);
+     if(curr_ptr > base_ptr + MAP_SIZE) 
+     {
+          fprintf(stderr, "Address out of range\n");
+          exit(1);
+     }
+     else
+     {
+          new_ptr = curr_ptr - sizeof(int);
+     }
+     int *out = (int *) new_ptr;
+     *out = 0;
+
+     int i, pid;
+     for (i = 0; i < nprod; i++) {
+          pid = fork();
+          if (pid < 0) {
+               printf("Producer forking error");
+               exit(1);
+          } else if (pid == 0) {
+               for(;;) {
+                    printf("Chef %c\n", i + 'A'); 
+                    sleep(5);
+               }
+          }
+     }
+
+     for (i = 0; i < ncons; i++) {
+          pid = fork();
+          if (pid < 0) {
+               printf("Consumer forking error");
+               exit(1);
+          } else if (pid == 0) {
+               for(;;) {
+                    printf("Consumer %c\n", i + 'A');
+                    sleep(5);
+               }
+          }
      }
      
-     struct cs1550_sem *sem = (struct cs1550_sem *) new_ptr;
-     sem->value = 0;
-     
+	
+
+/*
      printf("Base pointer (0x%08x), Current pointer (0x%08x), New pointer (0x%08x)\n", base_ptr, curr_ptr, new_ptr);
      printf("Base pointer (%d), Current pointer (%d), New pointer (%d)\n", *base_ptr, *curr_ptr, *new_ptr);
      cs1550_down(sem);
@@ -50,6 +149,7 @@ void main(int aegc, char *argv[])
      cs1550_up(sem);
      printf("Semaphore value (%d)\n", sem->value);
      printf("Base pointer (%d), Current pointer (%d), New pointer (%d)\n", *base_ptr, *curr_ptr, *new_ptr);
+*/
 
 }
 
